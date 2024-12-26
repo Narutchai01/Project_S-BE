cf1a6135b2021f6a0372b751870a2fb14f3940e8
Narutchai <koonfluk2015@gmail.com>
2024-12-26 16:35:23 +0700

    :safety_vest: create func gen jwt

--- utils/upload_image.go
+++ utils/upload_image.go
@@ -3,8 +3,10 @@ package utils
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/Narutchai01/Project_S-BE/config"
+	"github.com/golang-jwt/jwt"
 	storage_go "github.com/supabase-community/storage-go"
 )
 
@@ -36,4 +38,22 @@ func UploadImage(fileName string, dir string) (string, error) {
 	url := fmt.Sprintf("%s/object/public/pathfinder/%s", supa_api_url, fileName)
 
 	return url, nil
+}
+
+func CreateJWTToken(secretKey string, claims jwt.MapClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return "", fmt.Errorf("failed to sign token: %w", err)
+	}
+	return tokenString, nil
+}
+
+func GenerateToken(userID int) (string, error) {
+	secretKey := config.GetEnv("JWT_SECRET_KEY")
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 72).Unix(),
+	}
+	return CreateJWTToken(secretKey, claims)
 }
