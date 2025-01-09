f4e22627ce1ad806d28658f6c8a732423e5e1e00
Narutchai <koonfluk2015@gmail.com>
2025-01-09 15:11:18 +0700

    test commit message

--- routes/router.go
+++ routes/router.go
@@ -3,10 +3,18 @@ package routes
 import (
 	_ "github.com/Narutchai01/Project_S-BE/docs"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/swagger"
 	"gorm.io/gorm"
 )
 
+//	@title			Project S API
+//	@version		1.0
+//	@description	This is a sample server for Project S.
+//	@host			localhost:8080
+//	@BasePath		/api
+
 func Router(app *fiber.App, db *gorm.DB) {
+	app.Get("/swagger/*", swagger.HandlerDefault) // default
 	api := app.Group("/api")
 
 	api.Get("/", func(c *fiber.Ctx) error {
