b88a0325ac1f9984585117d4db375b69c8e299eb
achimicac <lagrongduke@gmail.com>
2025-01-20 23:57:38 +0700

    🧪 acne adapters

---
+++ test/acne/gorm_acne_repository_test.go
@@ -0,0 +1,259 @@
+package adapters
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	adapters "github.com/Narutchai01/Project_S-BE/adapters/acne"
+	"github.com/Narutchai01/Project_S-BE/entities"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func TestGormCreateAcne(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("An error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	repo := adapters.NewGormAcneRepository(gormDB)
+
+	expectData := entities.Acne{
+		Name: "innisfree",
+		Image: "innisfree/image/path",
+		CreateBY: 1,
+	}
+
+	t.Run("success", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectQuery(`INSERT INTO "acnes"`).WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+		mock.ExpectCommit()
+
+		_, err := repo.CreateAcne(expectData)
+
+		assert.NoError(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+	t.Run("failure", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectQuery(`INSERT INTO "acnes"`).WillReturnError(errors.New("database error"))
+		mock.ExpectRollback()
+
+		_, err := repo.CreateAcne(expectData)
+
+		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+}
+
+func TestGormGetAcnes(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("An error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	repo := adapters.NewGormAcneRepository(gormDB)
+
+	expectData := entities.Acne{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		Name: "innisfree",
+		Image: "innisfree/image/path",
+		CreateBY: 1,
+	}
+
+	columns := sqlmock.NewRows([]string{"id", "name", "image", "create_by"}).
+		AddRow(expectData.ID, expectData.Name, expectData.Image, expectData.CreateBY)
+	expectedSQL := `SELECT (.+) FROM "acnes"`
+
+	t.Run("success", func(t *testing.T) {
+		mock.ExpectQuery(expectedSQL).WillReturnRows(columns)
+
+		_, err := repo.GetAcnes()
+
+		assert.NoError(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+	t.Run("failure", func(t *testing.T) {
+		mock.ExpectQuery(expectedSQL).WillReturnError(errors.New("database error"))
+
+		_, err := repo.GetAcnes()
+
+		assert.Error(t, err)
+	})
+
+}
+
+func TestGormGetAcne(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("An error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	repo := adapters.NewGormAcneRepository(gormDB)
+
+	expectData := entities.Acne{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		Name: "innisfree",
+		Image: "innisfree/image/path",
+		CreateBY: 1,
+	}
+
+	expectedSQL := `SELECT \* FROM "acnes" WHERE "acnes"\."id" = \$1 AND "acnes"\."deleted_at" IS NULL ORDER BY "acnes"\."id" LIMIT \$2`
+	rows := sqlmock.NewRows([]string{"id", "name", "image", "create_by"}).
+		AddRow(expectData.ID, expectData.Name, expectData.Image, expectData.CreateBY)
+	t.Run("success", func(t *testing.T) {
+		mock.ExpectQuery(expectedSQL).
+			WithArgs(1, 1).
+			WillReturnRows(rows)
+
+		result, err := repo.GetAcne(1)
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectData, result)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+	t.Run("failure", func(t *testing.T) {
+		mock.ExpectQuery(expectedSQL).
+			WithArgs(1, 1).
+			WillReturnError(errors.New("database error"))
+
+		_, err := repo.GetAcne(int(expectData.ID))
+
+		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+}
+
+func TestGormUpdateUpdateAcne(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("An error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	repo := adapters.NewGormAcneRepository(gormDB)
+
+	expectData := entities.Acne{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		Name: "innisfree",
+		Image: "innisfree/image/path",
+		CreateBY: 1,
+	}
+
+	expectedSQL := `UPDATE "acnes" SET "id"=\$1,"updated_at"=\$2,"name"=\$3,"image"=\$4,"create_by"=\$5 WHERE id = \$6 AND "acnes"."deleted_at" IS NULL`
+
+	t.Run("success", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec(expectedSQL).
+			WithArgs(expectData.ID, sqlmock.AnyArg(), expectData.Name, expectData.Image, expectData.CreateBY, expectData.ID).
+			WillReturnResult(sqlmock.NewResult(0, 1))
+		mock.ExpectCommit()
+
+		result, err := repo.UpdateAcne(int(expectData.ID), expectData)
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectData, result)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+	t.Run("failure", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec(expectedSQL).
+		WithArgs(expectData.ID, sqlmock.AnyArg(), expectData.Name, expectData.Image, expectData.CreateBY, expectData.ID).
+			WillReturnError(errors.New("database error"))
+		mock.ExpectRollback()
+
+		_, err := repo.UpdateAcne(int(expectData.ID), expectData)
+
+		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+}
+
+func TestGormDeleteAcne(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("An error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	repo := adapters.NewGormAcneRepository(gormDB)
+
+	expectData := entities.Acne{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		Name: "innisfree",
+		Image: "innisfree/image/path",
+		CreateBY: 1,
+	}
+
+	expectedSQL := `UPDATE "acnes" SET "deleted_at"=\$1 WHERE "acnes"."id" = \$2 AND "acnes"."deleted_at" IS NULL`
+
+	t.Run("success", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec(expectedSQL).
+			WithArgs(sqlmock.AnyArg(), expectData.ID).
+			WillReturnResult(sqlmock.NewResult(0, 1))
+		mock.ExpectCommit()
+
+		err := repo.DeleteAcne(int(expectData.ID))
+
+		assert.NoError(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+	t.Run("failure", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectExec(expectedSQL).
+			WithArgs(sqlmock.AnyArg(), expectData.ID).
+			WillReturnError(errors.New("database error"))
+		mock.ExpectRollback()
+
+		err := repo.DeleteAcne(int(expectData.ID))
+
+		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+}
---
+++ test/acne/http_acne_repository_test.go
@@ -0,0 +1,439 @@
+package adapters_test
+
+import (
+	"bytes"
+	"errors"
+	"mime/multipart"
+	"net/http/httptest"
+
+	"testing"
+
+	adapters "github.com/Narutchai01/Project_S-BE/adapters/acne"
+	"github.com/Narutchai01/Project_S-BE/entities"
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"gorm.io/gorm"
+)
+
+type MockAcneService struct {
+	mock.Mock
+}
+
+// CreateAcne implements usecases.AcneUseCase.
+func (m *MockAcneService) CreateAcne(acne entities.Acne, file multipart.FileHeader, c *fiber.Ctx, token string) (entities.Acne, error) {
+	args := m.Called(acne, file, c)
+	return args.Get(0).(entities.Acne), args.Error(1)
+}
+
+func (m *MockAcneService) GetAcnes() ([]entities.Acne, error) {
+	args := m.Called()
+	return args.Get(0).([]entities.Acne), args.Error(1)
+}
+
+func (m *MockAcneService) GetAcne(id int) (entities.Acne, error) {
+	args := m.Called(id)
+	return args.Get(0).(entities.Acne), args.Error(1)
+}
+
+func (m *MockAcneService) UpdateAcne(id int, acne entities.Acne, file *multipart.FileHeader, c *fiber.Ctx) (entities.Acne, error) {
+	args := m.Called(id, acne, file, c)
+	return args.Get(0).(entities.Acne), args.Error(1)
+}
+
+func (m *MockAcneService) DeleteAcne(id int) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
+
+//Test
+func TestCreateAcneHandler(t *testing.T) {
+	setup := func() (*MockAcneService, *adapters.HttpAcneHandler, *fiber.App) {
+		mockService := new(MockAcneService)
+		handler := adapters.NewHttpAcneHandler(mockService)
+
+		app := fiber.New()
+		app.Post("/admin/acne", handler.CreateAcne)
+
+		return mockService, handler, app
+	}
+
+	expectData := entities.Acne{
+		Name: "innisfree",
+		Image: "innisfree/image/path",
+		CreateBY: 1,
+	}
+
+	t.Run("success", func(t *testing.T) {
+		mockService, _, app := setup()
+		mockService.On("CreateAcne",
+			mock.Anything,
+			mock.AnythingOfType("multipart.FileHeader"),
+			mock.Anything,
+		).Return(expectData, nil)
+
+		body := new(bytes.Buffer)
+		writer := multipart.NewWriter(body)
+		_ = writer.WriteField("name", expectData.Name)
+		part, _ := writer.CreateFormFile("file", "test.jpg")
+		part.Write([]byte("test image"))
+		writer.Close()
+
+		req := httptest.NewRequest("POST", "/admin/acne", body)
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+		req.Header.Set("token", "example-token")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusCreated, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("failed in body parser", func(t *testing.T) {
+		_, _, app := setup()
+		req := httptest.NewRequest("POST", "/admin/acne", bytes.NewBuffer([]byte("invalid body")))
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
+	})
+
+	t.Run("failed to get image file", func(t *testing.T) {
+		mockService, _, app := setup()
+		body := new(bytes.Buffer)
+		writer := multipart.NewWriter(body)
+		_ = writer.WriteField("name", expectData.Name)
+		writer.Close()
+
+		req := httptest.NewRequest("POST", "/admin/acne", body)
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("failed to create acne", func(t *testing.T) {
+		mockService, _, app := setup()
+		mockService.ExpectedCalls = nil
+		mockService.On("CreateAcne",
+			mock.Anything,
+			mock.AnythingOfType("multipart.FileHeader"),
+			mock.Anything,
+		).Return(entities.Acne{}, errors.New("service error"))
+
+		body := new(bytes.Buffer)
+		writer := multipart.NewWriter(body)
+		_ = writer.WriteField("name", expectData.Name)
+		part, _ := writer.CreateFormFile("file", "test.jpg")
+		part.Write([]byte("test image"))
+		writer.Close()
+
+		req := httptest.NewRequest("POST", "/admin/acne", body)
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+		req.Header.Set("token", "example-token")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusInternalServerError, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+}
+
+func TestGetAcnesHandler(t *testing.T) {
+	setup := func() (*MockAcneService, *adapters.HttpAcneHandler, *fiber.App) {
+		mockService := new(MockAcneService)
+		handler := adapters.NewHttpAcneHandler(mockService)
+
+		app := fiber.New()
+		app.Get("/acne", handler.GetAcnes)
+
+		return mockService, handler, app
+	}
+
+	expectData := []entities.Acne{
+		{
+			Model: gorm.Model{
+				ID: 1,
+			},
+			Name: "innisfree",
+			Image: "innisfree/image/path",
+			CreateBY: 1,
+		},
+		{
+			Model: gorm.Model{
+				ID: 2,
+			},
+			Name: "Dr.Pong",
+			Image: "drpong/image/path",
+			CreateBY: 1,
+		},
+	}
+
+	t.Run("success", func(t *testing.T) {
+		mockService, _, app := setup()
+		mockService.On("GetAcnes").Return(expectData, nil)
+
+		req := httptest.NewRequest("GET", "/acne", nil)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("failed to get admins", func(t *testing.T) {
+		mockService, _, app := setup()
+		mockService.ExpectedCalls = nil
+		mockService.On("GetAcnes").Return([]entities.Acne{}, errors.New("service error"))
+
+		req := httptest.NewRequest("GET", "/acne", nil)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusInternalServerError, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+}
+
+func TestGetAcneHandler(t *testing.T) {
+	setup := func() (*MockAcneService, *adapters.HttpAcneHandler, *fiber.App) {
+		mockService := new(MockAcneService)
+		handler := adapters.NewHttpAcneHandler(mockService)
+
+		app := fiber.New()
+		app.Get("/acne/:id", handler.GetAcne)
+
+		return mockService, handler, app
+	}
+
+	expectData := entities.Acne{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		Name: "innisfree",
+		Image: "innisfree/image/path",
+		CreateBY: 1,
+	}
+
+	t.Run("success", func(t *testing.T) {
+		mockService, _, app := setup()
+		mockService.On("GetAcne", int(expectData.ID)).Return(expectData, nil)
+
+		req := httptest.NewRequest("GET", "/acne/1", nil)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("failed to convert id to int", func(t *testing.T) {
+		mockService, _, app := setup()
+		req := httptest.NewRequest("GET", "/acne/error-id", nil)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("failed to get acne", func(t *testing.T) {
+		mockService, _, app := setup()
+		mockService.ExpectedCalls = nil
+		mockService.On("GetAcne", 1).Return(entities.Acne{}, errors.New("service error"))
+
+		req := httptest.NewRequest("GET", "/acne/1", nil)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusNotFound, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+}
+
+func TestUpdateAcneHandler(t *testing.T) {
+	setup := func() (*MockAcneService, *adapters.HttpAcneHandler, *fiber.App) {
+		mockService := new(MockAcneService)
+		handler := adapters.NewHttpAcneHandler(mockService)
+
+		app := fiber.New()
+		app.Put("/admin/acne/:id", handler.UpdateAcne)
+
+		return mockService, handler, app
+	}
+
+	expectData := entities.Acne{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		Name: "innisfree",
+		Image: "innisfree/image/path",
+		CreateBY: 1,
+	}
+
+	t.Run("success", func(t *testing.T) {
+		mockService, _, app := setup()
+		mockService.On("UpdateAcne",
+			mock.Anything,
+			mock.Anything,
+			mock.AnythingOfType("*multipart.FileHeader"),
+			mock.Anything,
+		).Return(expectData, nil)
+
+		body := new(bytes.Buffer)
+		writer := multipart.NewWriter(body)
+		_ = writer.WriteField("name", expectData.Name)
+		part, _ := writer.CreateFormFile("file", "test.jpg")
+		part.Write([]byte("test image"))
+		writer.Close()
+
+		req := httptest.NewRequest("PUT", "/admin/acne/1", body)
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+		req.Header.Set("token", "example-token")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("failed to convert id to int", func(t *testing.T) {
+		mockService, _, app := setup()
+		req := httptest.NewRequest("PUT", "/admin/acne/error-id", nil)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("failed in body parser", func(t *testing.T) {
+		_, _, app := setup()
+		req := httptest.NewRequest("PUT", "/admin/acne/1", bytes.NewBuffer([]byte("invalid body")))
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
+	})
+
+	t.Run("can upload if not provide image", func(t *testing.T) {
+		mockService, _, app := setup()
+		mockService.On("UpdateAcne",
+			mock.Anything,
+			mock.Anything,
+			mock.MatchedBy(func(file *multipart.FileHeader) bool {
+				return file == nil
+			}),
+			mock.Anything,
+		).Return(expectData, nil)
+
+		body := new(bytes.Buffer)
+		writer := multipart.NewWriter(body)
+		_ = writer.WriteField("name", expectData.Name) 
+		writer.Close()
+
+		req := httptest.NewRequest("PUT", "/admin/acne/1", body)
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+		req.Header.Set("token", "test-token")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("failed to update admin", func(t *testing.T) {
+		mockService, _, app := setup()
+		mockService.On("UpdateAcne",
+			mock.Anything,
+			mock.Anything,
+			mock.AnythingOfType("*multipart.FileHeader"),
+			mock.Anything,
+		).Return(entities.Acne{}, errors.New("service error"))
+
+		body := new(bytes.Buffer)
+		writer := multipart.NewWriter(body)
+		_ = writer.WriteField("name", expectData.Name)
+		part, _ := writer.CreateFormFile("file", "test.jpg")
+		part.Write([]byte("test image"))
+		writer.Close()
+
+		req := httptest.NewRequest("PUT", "/admin/acne/1", body)
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+		req.Header.Set("token", "test-token")
+
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusInternalServerError, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+}
+
+func TestDeleteAdminHandler(t *testing.T) {
+	setup := func() (*MockAcneService, *adapters.HttpAcneHandler, *fiber.App) {
+		mockService := new(MockAcneService)
+		handler := adapters.NewHttpAcneHandler(mockService)
+
+		app := fiber.New()
+		app.Delete("/admin/acne/:id", handler.DeleteAcne)
+
+		return mockService, handler, app
+	}
+
+
+	expectData := entities.Acne{
+		Model: gorm.Model{
+			ID: 1,
+		},
+	}
+
+	t.Run("success", func(t *testing.T) {
+		mockService, _, app := setup()
+		mockService.On("DeleteAcne", int(expectData.ID)).Return(nil)
+
+		req := httptest.NewRequest("DELETE", "/admin/acne/1", nil)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("failed to convert id to int", func(t *testing.T) {
+		mockService, _, app := setup()
+		req := httptest.NewRequest("DELETE", "/admin/acne/error-id", nil)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("failed to delete acne", func(t *testing.T) {
+		mockService, _, app := setup()
+		mockService.ExpectedCalls = nil
+		mockService.On("DeleteAcne", int(expectData.ID)).Return(errors.New("service error"))
+
+		req := httptest.NewRequest("DELETE", "/admin/acne/1", nil)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusNotFound, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+}
