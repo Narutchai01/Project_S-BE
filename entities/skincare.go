72125559fc66f2d9110eae2e756d960f0e9366fa
Narutchai <koonfluk2015@gmail.com>
2025-01-25 11:58:06 +0700

    🗃️add props to db

--- entities/skincare.go
+++ entities/skincare.go
@@ -7,7 +7,7 @@ import (
 type Skincare struct {
 	gorm.Model  `swaggerignore:"true"`
 	Image       string `json:"image" swaggerignore:"true"`
-	Name        string `json:"name"`
+	Name        string `json:"name" gorm:"not null unique"`
 	Description string `json:"description"`
 	CreateBY    uint   `reqHeader:"create_by" swaggerignore:"true"`
 }
--- entities/user.go
+++ entities/user.go
@@ -8,10 +8,10 @@ import (
 
 type User struct {
 	gorm.Model    `swaggerignore:"true"`
-	FullName      string    `json:"full_name"`
-	Email         string    `json:"email"`
-	Birthday      time.Time `json:"birthday"`
-	SensitiveSkin bool      `json:"sensitive_skin"`
+	FullName      string    `json:"full_name" gorm:"not null"`
+	Email         string    `json:"email" gorm:"unique not null"`
+	Birthday      time.Time `json:"birthday" gorm:"not null"`
+	SensitiveSkin bool      `json:"sensitive_skin" gorm:"not null"`
 	Password      string    `json:"password"`
 	Image         string    `json:"image" swaggerignore:"true"`
 }
