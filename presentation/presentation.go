02adffddcb7929026e1bafed331264d66fb5885c
Narutchai <koonfluk2015@gmail.com>
2025-01-28 20:51:18 +0700

    fix enities

--- presentation/presentation.go
+++ presentation/presentation.go
@@ -37,12 +37,12 @@ type Skincare struct {
 }
 
 type User struct {
-	ID            uint      `json:"id"`
-	FullName      string    `json:"full_name"`
-	Email         string    `json:"email"`
-	Birthday      time.Time `json:"birthday"`
-	SensitiveSkin bool      `json:"sensitive_skin"`
-	Image         string    `json:"image"`
+	ID            uint       `json:"id"`
+	FullName      string     `json:"full_name"`
+	Email         string     `json:"email"`
+	Birthday      *time.Time `json:"birthday"`
+	SensitiveSkin bool       `json:"sensitive_skin"`
+	Image         string     `json:"image"`
 }
 
 type Facial struct {
