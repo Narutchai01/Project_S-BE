d006d2494b12f162a4067ffcf04e52cb3bf208fd
achimicac <lagrongduke@gmail.com>
2025-01-29 18:31:43 +0700

    ✅ Middleware Unit Test

--- config/config.go
+++ config/config.go
@@ -3,14 +3,24 @@ package config
 import (
 	"log"
 	"os"
+	"testing"
 
 	"github.com/joho/godotenv"
 )
 
-func GetEnv(key string) string {
+func LoadEnv() {
+	if testing.Testing() { 
+		log.Println("Skipping .env loading in tests")
+		return
+	}
+
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Println("Warning: .env file not found, using system ENV")
 	}
+}
+
+
+func GetEnv(key string) string {
 	return os.Getenv(key)
 }
