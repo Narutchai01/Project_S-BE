072aa9d73da99bbf1dbe2c5c951382efed9f06c0
achimicac <lagrongduke@gmail.com>
2025-01-31 11:54:55 +0700

    ✅ unit test: getResultById adapters

--- test/result/gorm_result_repository_test.go
+++ test/result/gorm_result_repository_test.go
@@ -126,3 +126,65 @@ func TestGormGetResults(t *testing.T) {
 		assert.Error(t, err)
 	})
 }
+
+func TestGormGetResult(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("An error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	repo := adapters.NewGormResultRepository(gormDB)
+
+	expectData := entities.Result{
+		Model: gorm.Model{ID: 1},
+		Image: "image_url_test",
+		UserId: 1,
+		AcneType: []entities.Acne_Facial_Result{
+			{ID: 1, Count: 10},
+			{ID: 2, Count: 5},
+		},
+		FacialType: []entities.Acne_Facial_Result{
+			{ID: 1, Count: 10},
+			{ID: 2, Count: 5},
+		},
+		SkinType: 1,
+		Skincare: []uint{1, 2, 3},
+	}
+
+	// Convert slices to JSON
+	acneTypeJSON, _ := json.Marshal(expectData.AcneType)
+	facialTypeJSON, _ := json.Marshal(expectData.FacialType)
+	skincareJSON, _ := json.Marshal(expectData.Skincare)
+
+	expectedSQL := `SELECT \* FROM "results" WHERE "results"\."id" = \$1 AND "results"\."deleted_at" IS NULL ORDER BY "results"\."id" LIMIT \$2`
+	rows := sqlmock.NewRows([]string{"id", "image", "user_id", "acne_type", "facial_type", "skin_type", "skincare"}).
+	AddRow(expectData.ID, expectData.Image, expectData.UserId,  string(acneTypeJSON),  string(facialTypeJSON), expectData.SkinType,  string(skincareJSON))
+	t.Run("success", func(t *testing.T) {
+		mock.ExpectQuery(expectedSQL).
+			WithArgs(1, 1).
+			WillReturnRows(rows)
+
+		result, err := repo.GetResultById(int(expectData.ID))
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectData, result)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+	t.Run("failure", func(t *testing.T) {
+		mock.ExpectQuery(expectedSQL).
+			WithArgs(1, 1).
+			WillReturnError(errors.New("database error"))
+
+		_, err := repo.GetResultById(int(expectData.ID))
+
+		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+}
--- test/result/http_result_repository_test.go
+++ test/result/http_result_repository_test.go
@@ -183,3 +183,71 @@ func TestGetResultsHandler(t *testing.T) {
 		mockService.AssertExpectations(t)
 	})
 }
+
+func TestGetResultByIdHandler(t *testing.T) {
+	setup := func() (*MockResultService, *adapters.HttpResultHandler, *fiber.App) {
+		mockService := new(MockResultService)
+		handler := adapters.NewHttpResultHandler(mockService)
+
+		app := fiber.New()
+		app.Get("/result/:id", handler.GetResultById)
+
+		return mockService, handler, app
+	}
+
+	expectData := entities.Result{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		Image: "image_url_test",
+		UserId: 1,
+		AcneType: []entities.Acne_Facial_Result{
+			{ID: 1, Count: 10},
+			{ID: 2, Count: 5},
+		},
+		FacialType: []entities.Acne_Facial_Result{
+			{ID: 1, Count: 10},
+			{ID: 2, Count: 5},
+		},
+		SkinType: 1,
+		Skincare: []uint{1, 2, 3},
+	}
+
+	t.Run("success", func(t *testing.T) {
+		mockService, _, app := setup()
+		mockService.On("GetResultById", int(expectData.ID)).Return(expectData, nil)
+
+		req := httptest.NewRequest("GET", "/result/1", nil)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("failed to convert id to int", func(t *testing.T) {
+		mockService, _, app := setup()
+		req := httptest.NewRequest("GET", "/result/error-id", nil)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusBadRequest, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+
+	t.Run("failed to get skin", func(t *testing.T) {
+		mockService, _, app := setup()
+		mockService.ExpectedCalls = nil
+		mockService.On("GetResultById", int(expectData.ID)).Return(entities.Result{}, errors.New("service error"))
+
+		req := httptest.NewRequest("GET", "/result/1", nil)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := app.Test(req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, fiber.StatusNotFound, resp.StatusCode)
+		mockService.AssertExpectations(t)
+	})
+}
