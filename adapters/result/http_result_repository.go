9f6a143928d013311bf4ef15e401768cc06acb1b
achimicac <lagrongduke@gmail.com>
2025-02-02 22:31:38 +0700

    📝 update result service documentation

--- adapters/result/http_result_repository.go
+++ adapters/result/http_result_repository.go
@@ -15,6 +15,18 @@ func NewHttpResultHandler(resultUcase usecases.ResultUsecases) *HttpResultHandle
 	return &HttpResultHandler{resultUcase}
 }
 
+// CreateResult godoc
+//
+//	@Summary		Create  new result
+//	@Description	Create  new result
+//	@Tags			result
+//	@Accept			json
+//	@Produce		json
+//	@Param			result	body		entities.Result	true	"Result information"
+//	@Success		201		{object}	presentation.Responses
+//	@Failure		400		{object}	presentation.Responses
+//	@Failure		500		{object}	presentation.Responses
+//	@Router			/result [post]
 func (handler *HttpResultHandler) CreateResult(c *fiber.Ctx) error {
 
 	var result entities.Result
@@ -33,6 +45,16 @@ func (handler *HttpResultHandler) CreateResult(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusCreated).JSON(response)
 }
 
+// GetResults godoc
+//
+//	@Summary		Get results
+//	@Description	Get results
+//	@Tags			result
+//	@Accept			json
+//	@Produce		json
+//	@Success		200	{object}	presentation.Responses
+//	@Failure		500	{object}	presentation.Responses
+//	@Router			/result [get]
 func (handler *HttpResultHandler) GetResults(c *fiber.Ctx) error {
 	results, err := handler.resultUcase.GetResults()
 
@@ -43,6 +65,18 @@ func (handler *HttpResultHandler) GetResults(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(presentation.ResultsResponse(results))
 }
 
+// GetResultById godoc
+//
+//	@Summary		Get a result by ID
+//	@Description	Get a result by ID
+//	@Tags			result
+//	@Accept			json
+//	@Produce		json
+//	@Param			id	path		int	true	"Result ID"
+//	@Success		200	{object}	presentation.Responses
+//	@Failure		400	{object}	presentation.Responses
+//	@Failure		404	{object}	presentation.Responses
+//	@Router			/result/{id} [get]
 func (handler *HttpResultHandler) GetResultById(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 
@@ -59,6 +93,19 @@ func (handler *HttpResultHandler) GetResultById(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(presentation.ToResultResponse(result))
 }
 
+// UpdateResultById godoc
+//
+//	@Summary		Update a result by ID
+//	@Description	Update a result by ID
+//	@Tags			result
+//	@Accept			json
+//	@Produce		json
+//	@Param			result	body		entities.Result	true	"Result information"
+//	@Param			id	path		int	true	"Result ID"
+//	@Success		200		{object}	presentation.Responses
+//	@Failure		400		{object}	presentation.Responses
+//	@Failure		500		{object}	presentation.Responses
+//	@Router			/admin/result/ [put]
 func (handler *HttpResultHandler) UpdateResultById(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 
@@ -80,6 +127,18 @@ func (handler *HttpResultHandler) UpdateResultById(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(presentation.ToResultResponse(result))
 }
 
+// DeleteResultById godoc
+//
+//	@Summary		Delete a result by ID
+//	@Description	Delete a result by ID
+//	@Tags			result
+//	@Accept			json
+//	@Produce		json
+//	@Param			id	path		int	true	"Result ID"
+//	@Success		204	{object}	presentation.Responses
+//	@Failure		400	{object}	presentation.Responses
+//	@Failure		500	{object}	presentation.Responses
+//	@Router			/result/{id} [delete]
 func (handler *HttpResultHandler) DeleteResultById(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 
@@ -93,9 +152,22 @@ func (handler *HttpResultHandler) DeleteResultById(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusInternalServerError).JSON(presentation.ErrorResponse(err))
 	}
 
-	return c.Status(fiber.StatusOK).JSON(presentation.DeleteResponse(id))
+	return c.Status(fiber.StatusNoContent).JSON(presentation.DeleteResponse(id))
 }
 
+// GetResultsByUserIdFromToken godoc
+//
+//	@Summary		Get results by user_id from token
+//	@Description	Get results by user_id from token
+//	@Tags			result
+//	@Accept			json
+//	@Produce		json
+//	@Param			token	header		string	true	"User Bearer Token"
+//	@Param			result	body		entities.Result	true	"Result information"
+//	@Success		200		{object}	presentation.Responses
+//	@Failure		500		{object}	presentation.Responses
+//    @Failure		401		{object}	presentation.Responses
+//	@Router			/user/result [get]
 func (handler *HttpResultHandler) GetResultsByUserIdFromToken(c *fiber.Ctx) error {
 	token := c.Get("token")
 
@@ -108,6 +180,18 @@ func (handler *HttpResultHandler) GetResultsByUserIdFromToken(c *fiber.Ctx) erro
 	return c.Status(fiber.StatusOK).JSON(presentation.ResultsResponse(results))
 }
 
+// GetResultsByUserId godoc
+//
+//	@Summary		Get a result by user ID
+//	@Description	Get a result by user ID
+//	@Tags			result
+//	@Accept			json
+//	@Produce		json
+//	@Param			userId	path		int	true	"User ID"
+//	@Success		200	{object}	presentation.Responses
+//	@Failure		400	{object}	presentation.Responses
+//	@Failure		404	{object}	presentation.Responses
+//	@Router			/result/user/{userId} [get]
 func (handler *HttpResultHandler) GetResultsByUserId(c *fiber.Ctx) error {
 
 	user_id, err := c.ParamsInt("userId")
@@ -119,12 +203,24 @@ func (handler *HttpResultHandler) GetResultsByUserId(c *fiber.Ctx) error {
 	results, err := handler.resultUcase.GetResultsByUserId(user_id)
 
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(presentation.ErrorResponse(err))
+		return c.Status(fiber.StatusNotFound).JSON(presentation.ErrorResponse(err))
 	}
 
 	return c.Status(fiber.StatusOK).JSON(presentation.ResultsResponse(results))
 }
 
+// GetLatestResultByUserIdFromToken godoc
+//
+//	@Summary		Get latest result by user_id from token
+//	@Description	Get latest result by user_id from token
+//	@Tags			result
+//	@Accept			json
+//	@Produce		json
+//	@Param			token	header		string	true	"User Bearer Token"
+//	@Success		200		{object}	presentation.Responses
+//	@Failure		500		{object}	presentation.Responses
+//    @Failure		401		{object}	presentation.Responses
+//	@Router			/user/result [get]
 func (handler *HttpResultHandler) GetLatestResultByUserIdFromToken(c *fiber.Ctx) error {
 	token := c.Get("token")
 
--- entities/result.go
+++ entities/result.go
@@ -7,7 +7,7 @@ import (
 type Result struct {
 	gorm.Model `swaggerignore:"true"`
 	Image      string               `json:"image" gorm:"not null"`
-	UserId     uint                 `json:"user_id" gorm:"not null" swaggerignore:"true"`
+	UserId     uint                 `json:"user_id" gorm:"not null"`
 	AcneType   []Acne_Facial_Result `json:"acne_type" gorm:"type:jsonb;serializer:json"`
 	FacialType []Acne_Facial_Result `json:"facial_type" gorm:"type:jsonb;serializer:json"`
 	SkinType   uint                 `json:"skin_type" gorm:"not null"`
@@ -15,6 +15,6 @@ type Result struct {
 }
 
 type Acne_Facial_Result struct {
-	ID    uint `json:"id" swaggerignore:"true"`
+	ID    uint `json:"id"`
 	Count uint `json:"count"`
 }
