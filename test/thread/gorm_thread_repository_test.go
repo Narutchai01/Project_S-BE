97ce473e3e5037bea533da51a3ef688429e455fe
Narutchai <koonfluk2015@gmail.com>
2025-02-21 02:25:43 +0700

    ✅ improve unit gorm Create Bookmark

--- test/thread/gorm_thread_repository_test.go
+++ test/thread/gorm_thread_repository_test.go
@@ -319,3 +319,48 @@ func TestDeleteThreadGorm(t *testing.T) {
 	})
 
 }
+func TestCreateBookmark(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	defer db.Close()
+
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	repo := adapters.NewGormThreadRepository(gormDB)
+	t.Run("CreateBookmark", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO "bookmarks" ("created_at","updated_at","deleted_at","thread_id","user_id","status") VALUES ($1,$2,$3,$4,$5,$6)`)).
+			WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), 1, 1, true).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+		mock.ExpectCommit()
+
+		bookmark, err := repo.CreateBookmark(1, 1)
+
+		assert.NoError(t, err)
+		assert.Equal(t, uint(1), bookmark.ThreadID)
+		assert.Equal(t, uint(1), bookmark.UserID)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+	t.Run("CreateBookmark Error", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO "bookmarks" ("created_at","updated_at","deleted_at","thread_id","user_id","status") VALUES ($1,$2,$3,$4,$5,$6)`)).
+			WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), 1, 1, true).
+			WillReturnError(gorm.ErrInvalidData)
+		mock.ExpectRollback()
+
+		bookmark, err := repo.CreateBookmark(1, 1)
+
+		assert.Error(t, err)
+		assert.Equal(t, uint(0), bookmark.ThreadID)
+		assert.Equal(t, uint(0), bookmark.UserID)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+}
