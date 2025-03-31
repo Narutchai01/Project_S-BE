e57e16fb3daccfa156608036f46d603ecebed1c8
Narutchai <koonfluk2015@gmail.com>
2025-03-31 15:13:37 +0700

    👍

--- entities/skincare.go
+++ entities/skincare.go
@@ -7,10 +7,10 @@ import (
 
 type Skincare struct {
 	gorm.Model  `swaggerignore:"true"`
-	Image       string          `json:"image" swaggerignore:"true"`
+	Image       string          `json:"image" swaggerignore:"true" gorm:"default:null"`
 	Name        string          `json:"name" gorm:"not null unique"`
-	Description string          `json:"description"`
+	Description string          `json:"description" gorm:"default:null"`
 	CreateBY    uint            `reqHeader:"create_by" swaggerignore:"true"`
 	Admin       Admin           `gorm:"foreignKey:CreateBY;references:ID"`
-	Embedding   pgvector.Vector `gorm:"type:vector(384)"`
+	Embedding   pgvector.Vector `gorm:"type:vector(384);default:null"`
 }
