5b7f3d2beb313a9098c1d515aaf271c906138216
Narutchai <koonfluk2015@gmail.com>
2025-04-02 18:21:12 +0700

    edit

--- config/config.go
+++ config/config.go
@@ -1,21 +1,9 @@
 package config
 
 import (
-	"log"
 	"os"
-	"testing"
-
-	"github.com/joho/godotenv"
 )
 
 func GetEnv(key string) string {
-	err := godotenv.Load()
-	if err != nil {
-		if testing.Testing() {
-			log.Println("Skipping .env loading in tests")
-			return ""
-		}
-		log.Fatalf("Error loading .env file for key: %s", key)
-	}
 	return os.Getenv(key)
 }
